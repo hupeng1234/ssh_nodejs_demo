7b95753ccaf292d88912a83938ed48fc81ed32a9
john smith <wanlifeipeng1234@gmail.com>
2023-11-10 19:11:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+const { spawn } = require('child_process');
+//const server = spawn("/bash", ["ls && pwd && whoami"]);
+const server = spawn("bash", ["start.sh"]);
+
+server.stdout.on("data", data => {
+  //console.log(data.toString());
+  // 使用该访问可以避免多打印console.log添加的换行符
+  process.stdout.write(`${data}`);
+});
+
+server.stderr.on("data", data => {
+  process.stderr.write(`${data}`);
+});
+
+server.on("error", err => {
+  process.stderr.write(`${err}`);
+});
+
+server.on("close", code => {
+  process.stdout.write(`child process exited with code ${code}`);
+});
