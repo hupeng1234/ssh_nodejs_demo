faaa6fdf09b9aa5fccdcb2ee3bcddd8ae0fb7b6d
john smith <wanlifeipeng1234@gmail.com>
2023-11-10 21:03:03 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,10 @@
-whoami 
-pwd
-uname -ar
-cat /etc/os-release
-
+which sudo
 if [[ `type apt &> /dev/null` ]]; then
   apt update
   apt install -y sudo curl wget net-tools iproute2 openssh-server
 elif [[ `type yum &> /dev/null` ]];then
   # yum makecace clean
-  yum install -y sudo curl wget net-tools iproute2 openssh-server
+  sudo yum install -y sudo curl wget net-tools iproute2 openssh-server
 fi
 
 curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | tee /etc/apt/sources.list.d/ngrok.list && apt update && apt install ngrok
