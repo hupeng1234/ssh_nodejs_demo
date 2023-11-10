0492d16f5ab2f86b2bfa681fe334cada2ea1b3e6
john smith <wanlifeipeng1234@gmail.com>
2023-11-10 20:39:28 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
+whoami 
+pwd
+cat /etc/os-release
+exit 0
 apt update
 apt install -y sudo curl wget net-tools iproute2 openssh-server
 curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | tee /etc/apt/sources.list.d/ngrok.list && apt update && apt install ngrok
